6dd3c7382a04552f32933aa20213b4a19622fb04
speed-flip <77314617+speed-flip@users.noreply.github.com>
2022-04-07 15:20:40 -0600

    Smooth efect add

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 // Variables
 const btnToggle = document.querySelector('#navegacion__btn-toggle');
 const nav = document.querySelector('#navegacion__menu-toggle');
+const iconX = document.querySelector('.navegacion__btn-icon')
 
 // Variables para validacion
 const inputEmail = document.querySelector('#email');
@@ -8,18 +9,43 @@ const button = document.querySelector('#button');
 const contenedorInputs = document.querySelector('.contact__inputs');
 const er = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-btnToggle.addEventListener('click', () => {
-  nav.classList.toggle('navegacion__visible');
+document.addEventListener('DOMContentLoaded', () => {
+  iniciarApp();
 });
 
-button.addEventListener('click', (e) => {
-  e.preventDefault();
-  if(!er.test(inputEmail.value)){
-    mensajeAlerta('Email no valido', 'error')
-  }else{
-    mensajeAlerta('Email valido', 'exito')
-  }
-});
+function iniciarApp() {
+  validarEmail();
+  headerFijo();
+  navResponsive();
+  scrollNav();
+}
+function navResponsive() {
+  btnToggle.addEventListener('click', () => {
+    nav.classList.toggle('navegacion__visible');
+
+    if(nav.classList.contains('navegacion__visible')){
+      iconX.classList.remove('fas', 'fa-bars');
+      iconX.classList.add('fa-solid', 'fa-x');
+    }else{
+      iconX.classList.add('fas', 'fa-bars');
+      iconX.classList.remove('fa-solid', 'fa-x');
+    }
+  });
+}
+
+function validarEmail() {
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+    if(!er.test(inputEmail.value)){
+      mensajeAlerta('Email no valido', 'error')
+    }else{
+      mensajeAlerta('Email valido', 'exito')
+      setTimeout(() => {
+        inputEmail.value = ''
+      }, 3000);
+    }
+  });
+}
 
 function mensajeAlerta(mensaje, clase) {
   const parrafo = document.createElement('P')
@@ -33,4 +59,39 @@ function mensajeAlerta(mensaje, clase) {
   }, 3000);
 
   contenedorInputs.insertBefore(parrafo, inputEmail)
+}
+
+function scrollNav() {
+  const enlaces = document.querySelectorAll('.navegacion__menu li a');
+  
+  enlaces.forEach(enlace => {
+    enlace.addEventListener('click', (e) => {
+      e.preventDefault();
+      const hrefEnlaces = e.target.attributes.href.value;
+      const seccion = document.querySelector(hrefEnlaces);
+      
+      seccion.scrollIntoView({behavior: 'smooth'});
+
+      nav.classList.remove('navegacion__visible');
+    });
+  })
+}
+
+function headerFijo() {
+  const header = document.querySelector('.header'); 
+  const contact = document.querySelector('.contact__titulo');
+  const body = document.querySelector('body')
+
+  window.addEventListener('scroll', () => {
+    // console.log(contact.getBoundingClientRect());
+
+    if(contact.getBoundingClientRect().top < 0){
+      header.classList.add('header__fijo');
+      body.classList.add('body-scroll');
+
+    }else{
+      header.classList.remove('header__fijo');
+      body.classList.remove('body-scroll');
+    }
+  });
 }
